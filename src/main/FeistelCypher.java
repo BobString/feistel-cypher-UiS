68ab1e9331eb60b5926efb90098c060bf4abdd29
Roberto Martin <robert.90@gmail.com>
2013-09-01 19:34:42 +0200

    Factory pattern done.

--- src/main/FeistelCypher.java
+++ src/main/FeistelCypher.java
@@ -4,104 +4,24 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 
-/**
- * Feistel cypher based XTEA
- * 
- * @author roberto
- * 
- */
-public class FeistelCypher {
-	/**
-	 * 32 cycles = 64 feistel rounds
-	 */
-	private int numCycles = 32;
+public abstract class FeistelCypher {
 
-	/**
-	 * 128 bits key, 4 integers
-	 */
-	private int[] key = new int[4];
-	/**
-	 * Used in each round
-	 */
-	private int delta = 0x9E3779B9;
+	private FeistelCypherType type = null;
 
-	/**
-	 * Constructor of the key
-	 * 
-	 * @param key
-	 */
-	public FeistelCypher(String key) {
-		byte[] keybyte = key.getBytes();
-		if (keybyte == null) {
-			throw new RuntimeException("No key");
-		} else if (keybyte.length < 16) {
-			byte[] newkey = new byte[16];
-			for (int i = 0; i < 16; i++) {
-				newkey[i] = keybyte[i % 16];
-			}
-			keybyte = newkey;
-		}
-
-		// Change bytes for ints
-		IntBuffer intBuf = ByteBuffer.wrap(keybyte).order(ByteOrder.BIG_ENDIAN)
-				.asIntBuffer();
-		int[] array = new int[intBuf.remaining()];
-		intBuf.get(array);
-		for (int i = 0; i < 4; i++) {
-			this.key[i] = array[i];
-		}
+	public FeistelCypher(FeistelCypherType type) {
+		this.setType(type);
 
 	}
 
-	public byte[] encrypt(String plainText) {
-		byte[] text = plainText.getBytes();
-		int[] intText = byte2int(text);
-
-		int i, v0, v1, sum, n;
-		i = 0;
-		System.out.println(intText.length % 2);
-		while (i < intText.length) {
-			n = numCycles;
-			v0 = intText[i];
-			v1 = intText[i + 1];
-			sum = 0;
-			for (int ind = 0; ind < n; ind++) {
-				v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-				sum += delta;
-				v1 += (((v0 << 4) ^ (v0 >> 5)) + v0)
-						^ (sum + key[(sum >> 11) & 3]);
-			}
-			intText[i] = v0;
-			intText[i + 1] = v1;
-			i += 2;
-		}
-		return int2byte(intText);
+	public FeistelCypherType getType() {
+		return type;
 	}
 
-	public String decrypt(byte[] encondeMessage) {
-		int[] intText = byte2int(encondeMessage);
-
-		int i, v0, v1, sum, n;
-		i = 0;
-		while (i < intText.length) {
-			n = numCycles;
-			v0 = intText[i];
-			v1 = intText[i + 1];
-			sum = delta * numCycles;
-			for (int ind = 0; ind < n; ind++) {
-				v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0)
-						^ (sum + key[(sum >> 11) & 3]);
-				sum -= delta;
-				v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-			}
-			intText[i] = v0;
-			intText[i + 1] = v1;
-			i += 2;
-		}
-		return new String(int2byte(intText));
+	public void setType(FeistelCypherType type) {
+		this.type = type;
 	}
 
-	private int[] byte2int(byte[] source) {
+	protected int[] byte2int(byte[] source) {
 		IntBuffer intBuf = ByteBuffer.wrap(source).order(ByteOrder.BIG_ENDIAN)
 				.asIntBuffer();
 		int[] array = new int[intBuf.remaining()];
@@ -109,11 +29,18 @@ public class FeistelCypher {
 		return array;
 	}
 
-	private byte[] int2byte(int[] source) {
+	protected byte[] int2byte(int[] source) {
 
 		ByteBuffer byteBuffer = ByteBuffer.allocate(source.length * 4);
 		IntBuffer intBuffer = byteBuffer.asIntBuffer();
 		intBuffer.put(source);
 		return byteBuffer.array();
 	}
+
+	public abstract byte[] encrypt(String plainText);
+
+	public abstract String decrypt(byte[] cryptText);
+
+	public abstract void setKey(String key);
+
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,21 +1,25 @@
 package test;
 
 import main.FeistelCypher;
+import main.FeistelCypherFactory;
+import main.FeistelCypherType;
 
 public class Test {
 	/* Simple usage example */
 	public static String quote = "OLA K ASE";
 
 	public static void main(String[] args) {
-		/* Create a cipher using the first 16 bytes of the passphrase */
-		FeistelCypher tea = new FeistelCypher(
-				"And is there honey still for tea?");
+		
+		/* XTEA */
+		FeistelCypher xtea = FeistelCypherFactory
+				.getFeistelCypher(FeistelCypherType.XTEA);
+		xtea.setKey("And is there honey still for tea?");
 
-		/* Run it through the cipher... and back */
-		byte[] crypt = tea.encrypt(quote);
-		String result = tea.decrypt(crypt);
+		/* Run it through the cipher and back */
+		byte[] crypt = xtea.encrypt(quote);
+		String result = xtea.decrypt(crypt);
 
-		/* Ensure that all went well */
+		/* Lets see that all went well */
 		String test = new String(result);
 		if (!test.equals(quote)) {
 			System.out.println("No son iguales:");
